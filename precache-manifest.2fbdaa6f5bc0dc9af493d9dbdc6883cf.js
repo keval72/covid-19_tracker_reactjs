25dbb1c5128a53ed33f440d08e951b935f98c505
keval <ekevalpatel@gmail.com>
2020-09-27 19:14:30 +0530

    Updates

---
+++ precache-manifest.2fbdaa6f5bc0dc9af493d9dbdc6883cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6150318f50739058c3cce1bb177c1a82",
+    "url": "/covid-19_tracker_reactjs/index.html"
+  },
+  {
+    "revision": "99f3833f69b91784d848",
+    "url": "/covid-19_tracker_reactjs/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "3a116d4df7465c9750d1",
+    "url": "/covid-19_tracker_reactjs/static/css/main.19053c0f.chunk.css"
+  },
+  {
+    "revision": "99f3833f69b91784d848",
+    "url": "/covid-19_tracker_reactjs/static/js/2.9efd493c.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid-19_tracker_reactjs/static/js/2.9efd493c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a116d4df7465c9750d1",
+    "url": "/covid-19_tracker_reactjs/static/js/main.03e32c89.chunk.js"
+  },
+  {
+    "revision": "bfaa845a55ad0adbfc3a",
+    "url": "/covid-19_tracker_reactjs/static/js/runtime-main.277b75d5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19_tracker_reactjs/static/media/image.d7265326.png"
+  }
+]);
